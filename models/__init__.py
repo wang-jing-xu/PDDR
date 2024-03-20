46f5e00494fe3b8edc71026e8a35ba1d0fe3834b
Jingxu Wang <74650916+wang-jing-xu@users.noreply.github.com>
2024-03-20 11:03:07 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,12 @@
+from __future__ import absolute_import
 
+# from .ResNet import *
+# # from .VGG import *
+# # from .DenseNet import *
+# from .AlexNet import *
+# from .MnasNet import *
+# from .MobileNet import *
+# # from .ResNeSt import *
+# from .Ablation import *
+from .Deploy import *
+from .Run import *
