10d6edcea59cfe7c4f0108ac6b8b748a52dbd267
Jingxu Wang <74650916+wang-jing-xu@users.noreply.github.com>
2024-03-20 11:03:43 +0800

    Update __init__.py

--- models/Run/__init__.py
+++ models/Run/__init__.py
@@ -1,1 +1,64 @@
+from __future__ import absolute_import
+
+from .resnet_new1 import *
+from .resnet_new3 import *
+
+# from .resnet_old import *
+# from .resnet_ac import *
+# from .resnet_double import *
+# from .resnet_triple import *
+# from .resnet_dist import *
+# from .resnet_dist4 import *
+# from .resnet_dist5 import *
+# from .resnet_dist6 import *
+# from .resnet_dist7 import *
+# from .resnet_dist8 import *
+# from .resnet_dist9 import *
+# from .resnet_dist10 import *
+#
+# from .resnet_group1 import *
+# from .resnet_group2 import *
+# from .resnet_group3 import *
+# from .resnet_group4 import *
+# from .resnet_dilate1 import *
+# from .resnet_combine1 import *
+# from .resnet_combine2 import *
+# from .resnet_double2 import *
+# from .resnet_double3 import *
+# from .resnet_triple2 import *
+# from .resnet_combine3 import *
+# from .resnet_combine4 import *
+# from .resnet_combine5 import *
+# from .resnet_validat1 import *
+# from .resnet_validat2 import *
+# from .resnet_combine6 import *
+# from .resnet_combine7 import *
+# from .resnet_combine8 import *
+# from .resnet_combine9 import *
+# from .resnet_combine10 import *
+# from .resnet_combine11 import *
+# from .resnet_combine12 import *
+# from .resnet_combine13 import *
+# from .resnet_validate3 import *
+# from .resnet_se import *
+# from .resnet_ge import *
+# from .resnet_combine14 import *
+# from .resnet_combine15 import *
+# from .resnet_combine16 import *
+# from .resnet_combine17 import *
+# from .resnet_combine18 import *
+# from .resnet_combine19 import *
+# from .resnet_combine20 import *
+# from .resnet_validate4 import *
+# from .resnet_validate5 import *
+# from .resnet_validate6 import *
+
+# from .resnet_new1 import *
+# from .resnet_new2 import *
+# from .resnet_new3 import *
+# from .resnet_new4 import *
+# from .resnet_new1_se import *
+# from .resnet_new3_se import *
+# from.resnet_lottery import *
+# from.resnet_lottery2 import *
 
